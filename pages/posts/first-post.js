b8402d7a05fc4d0b8a7d04c95c5169198d9a8c83
drewkeat <drewjkeat@gmail.com>
2022-09-24 12:07:10 -0600

    include fb sdk via next/Script

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Link from "next/link";
+import Script from "next/script";
 
 export default function FirstPost() {
   return (
@@ -7,6 +8,16 @@ export default function FirstPost() {
       <Head>
         <title>First Post</title>
       </Head>
+      {/* strategy controls when the third-party script should load. 
+        A value of lazyOnload tells Next.js to load this particular script lazily during browser idle time.
+        onLoad is used to run any JavaScript code immediately after the script has finished loading. In this example, we log a message to the console that mentions that the script has loaded correctly */}
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      />
       <h1>First Post</h1>
       <h2>
         {" "}
